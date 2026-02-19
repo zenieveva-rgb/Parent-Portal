2ee5bda60d4414bd65d4280d7ff93ec66f816b53
zenieveva-rgb <zenieveva@gmail.com>
2026-02-19 09:09:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -101,12 +101,28 @@ function processData(searchTerm = "") {
                 if (!albums[sName]) albums[sName] = { grade: val.grade, logs: [] };
                 albums[sName].logs.push({ key, ...val });
             });
+
             Object.keys(albums).filter(n => n.toLowerCase().includes(searchTerm)).forEach(name => {
+                const studentLogs = albums[name].logs;
+                const lastScan = studentLogs[studentLogs.length - 1].time || studentLogs[studentLogs.length - 1].scannedAt || '--:--';
+                
                 const wrapper = document.createElement('div');
                 wrapper.className = 'album-row-wrapper';
                 const checkboxHTML = isSelectionMode ? `<input type="checkbox" class="album-checkbox" onchange="toggleSelect('${name}')" ${selectedAlbums.has(name) ? 'checked' : ''}>` : '';
-                wrapper.innerHTML = `${checkboxHTML}<div class="student-album"><div class="album-icon"><i class="fa-solid fa-folder"></i></div><div class="album-name">${name}</div><div class="total-scans">${albums[name].logs.length} Scans</div></div>`;
-                wrapper.querySelector('.student-album').onclick = () => { if (!isSelectionMode) showPopUp(name, albums[name].logs); };
+                
+                wrapper.innerHTML = `
+                    ${checkboxHTML}
+                    <div class="student-album">
+                        <div class="album-icon"><i class="fa-solid fa-folder-open"></i></div>
+                        <div class="album-info-meta">
+                            <div class="album-name">${name}</div>
+                            <div class="album-sub">Grade: ${albums[name].grade || 'N/A'} • Last seen: ${lastScan}</div>
+                        </div>
+                        <div class="total-scans">${studentLogs.length} Logs</div>
+                    </div>
+                `;
+                
+                wrapper.querySelector('.student-album').onclick = () => { if (!isSelectionMode) showPopUp(name, studentLogs); };
                 historyTable.appendChild(wrapper);
             });
         }
